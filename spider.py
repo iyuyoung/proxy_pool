c55824100b326c124a66885799ca446458e1a36a
yuyoung <892887001@qq.com>
2019-01-21 15:45:01 +0800

    修改注释

--- spider.py
+++ spider.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-from new.untils.RedisClient import r
+from conn import r
 
 
 class proxy():
@@ -11,20 +11,21 @@ class proxy():
             'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.62 Safari/537.36'
         }
 
-    '''
-    获取网站源代码
-    '''
-
     def getContent(self):
+        '''
+        获取网站源代码
+        :return:
+        '''
         url = 'http://www.goubanjia.com/'
         data = requests.get(url, headers=self.headers)
         self.parse(data.text)
 
-    '''
-    解析源代码
-    '''
-
     def parse(self, data):
+        '''
+        解析源代码
+        :param data:
+        :return:
+        '''
         soup = BeautifulSoup(data, 'html.parser')
         result = soup.find_all('td', class_='ip')
         for x in result:
@@ -35,11 +36,12 @@ class proxy():
             res = re.sub(data, '', str(x))
             r.add(self.key, res + str(port))
 
-    '''
-    解密端口号
-    '''
-
     def parse_port(self, port):
+        '''
+        解密端口号
+        :param port:
+        :return:
+        '''
         string = 'ABCDEFGHIZ'
         arr = list(port)
         lists = []
@@ -49,11 +51,11 @@ class proxy():
         ports = ''.join(str(x) for x in lists)
         return int(ports) >> 3
 
-    '''
-    随机获取代理地址
-    '''
-
     def random(self):
+        '''
+        随机获取代理地址
+        :return:
+        '''
         url = 'https://www.baidu.com'
         value = r.random(self.key)
         if value is None:
@@ -75,5 +77,5 @@ class proxy():
 
 proxy = proxy()
 
-ip=proxy.random()
+ip = proxy.random()
 print(ip)
